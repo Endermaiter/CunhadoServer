ce06b952e38977a889d578239640621da14484d6
Endermaiter <mpereznovoa@danielcastelao.org>
2023-02-10 18:29:33 +0100

    Opciones restantes acabadas. Trabajo acabado

--- src/Server.java
+++ src/Server.java
@@ -41,20 +41,42 @@ public class Server {
                     case 1:
                         byte[] arrayDatos = new byte[digitos];
                         is.read(arrayDatos);
+
                         int datoRecibido = Integer.parseInt(new String(arrayDatos));
-                        System.out.println("DATO RECIBIDO: " + datoRecibido);
-                        float eqCamposInt = datoRecibido/4050f;
-                        System.out.println("EqCamposINT: "+eqCamposInt);
-                        String eqCampos = String.valueOf(eqCamposInt); //1 campo de fútbol = 4050m2
-                        System.out.println("EqCampos: "+eqCampos);
+                        float eqCamposFlo = datoRecibido / 4050f; //1 campo de fútbol = 4050m2
+                        String eqCampos = String.valueOf(eqCamposFlo);
+
                         devolverDatos(eqCampos);
                         break;
                     case 2:
-                        System.out.println("OPTION 2");
+                        byte[] arrayDatos2 = new byte[digitos];
+                        is.read(arrayDatos2);
+
+                        int datoRecibido2 = Integer.parseInt(new String(arrayDatos2));
+                        float eqMesesFlo = (datoRecibido2 / 575f); //Me quedan 575 meses para la jubilación ((67-19)*12)
+                        String eqMeses = String.valueOf(eqMesesFlo);
+
+                        devolverDatos(eqMeses);
                         break;
                     case 3:
+                        byte[] arrayDatos3 = new byte[digitos];
+                        is.read(arrayDatos3);
+
+                        int datoRecibido3 = Integer.parseInt(new String(arrayDatos3));
+                        float eqLibrosFlo = datoRecibido3 / 47f; //Pérez Reverte tiene 47 obras literarias
+                        String eqLibros = String.valueOf(eqLibrosFlo);
+
+                        devolverDatos(eqLibros);
                         break;
                     case 4:
+                        byte[] arrayDatos4 = new byte[digitos];
+                        is.read(arrayDatos4);
+
+                        int datoRecibido4 = Integer.parseInt(new String(arrayDatos4));
+                        float difGasolinaFlo = datoRecibido4 - 1.517f; //gasolinera más barata -> 1,517€/L
+                        String difGasolina = String.valueOf(difGasolinaFlo);
+
+                        devolverDatos(difGasolina);
                         break;
                     case 5:
                         System.out.println("Cerrando el nuevo socket");
